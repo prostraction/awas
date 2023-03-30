6af3a351782732e6e056d16d667d73e5558ce182
prostraction <47314760+prostraction@users.noreply.github.com>
2023-03-31 00:27:02 +0300

    Update packages and make simple form

---
+++ src/AppContent.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class AppContent extends Component {
+    render() {
+        return (
+            <p>Content!
+                <br/> <button className="btn btn-primary" href='#'>Button!</button>
+            </p>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from 'react';
+import ReactDOM, {createRoot} from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import AppFooter from './AppFooter';
+import AppContent from './AppContent';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css'
+
+class App extends Component {
+  // every react component must have a render() function
+  render() {
+    return (
+      <div className="app">
+        <div>
+          <h1>hello?</h1>
+          <AppContent />
+        </div>
+        <AppFooter />
+      </div>
+    );
+  }
+}
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
