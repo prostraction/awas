07e327ab1a231fa83b39851245ddcad2386cf35b
prostraction <47314760+prostraction@users.noreply.github.com>
2023-04-01 22:54:06 +0300

    Testing React

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import ReactDOM, {createRoot} from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 
+import AppHeader from './AppHeader';
 import AppFooter from './AppFooter';
 import AppContent from './AppContent';
 
@@ -8,15 +9,28 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handlePostChange = this.handlePostChange.bind(this);
+    this.state = {posts: []};
+  }
+
+  handlePostChange(posts) {
+    this.setState({posts: posts});
+  }
+
   // every react component must have a render() function
   render() {
+    const myProps = {
+      title: "Hola!!",
+      subject: "123"
+    };
     return (
       <div className="app">
-        <div>
-          <h1>hello?</h1>
-          <AppContent />
-        </div>
-        <AppFooter />
+        <AppHeader {...myProps} posts={this.state.posts} handlePostChange={this.handlePostChange}/>
+         <AppContent handlePostChange = {this.handlePostChange} posts={this.state.posts}/>
+         <AppFooter/>
       </div>
     );
   }
