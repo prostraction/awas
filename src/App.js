9fb2434dcaeb7f788809eb4981cc2a4fe0102143
prostraction <47314760+prostraction@users.noreply.github.com>
2023-04-02 13:55:39 +0300

    Clean proejct

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import './index.css'
+
+export default class App extends Component {
+    render() {
+        return (
+            <h1> ok </h1>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,41 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {createRoot} from 'react-dom/client'
 
-import AppHeader from './AppHeader';
-import AppFooter from './AppFooter';
-import AppContent from './AppContent';
-
-import 'bootstrap/dist/css/bootstrap.min.css'
+import App from './App'
 import './index.css'
 
+/*
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.handlePostChange = this.handlePostChange.bind(this);
-    this.state = {posts: []};
-  }
-
-  handlePostChange(posts) {
-    this.setState({posts: posts});
-  }
-
   // every react component must have a render() function
   render() {
-    const myProps = {
-      title: "Hola!!",
-      subject: "123"
-    };
     return (
       <div className="app">
-        <AppHeader {...myProps} posts={this.state.posts} handlePostChange={this.handlePostChange}/>
-         <AppContent handlePostChange = {this.handlePostChange} posts={this.state.posts}/>
-         <AppFooter/>
+         <App/>
       </div>
     );
   }
 }
-
+*/
 //ReactDOM.render(<App />, document.getElementById('root'));
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
