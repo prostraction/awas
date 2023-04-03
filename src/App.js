1f0311921e9c59565960f3d8a3e40e97a5972dbd
prostraction <47314760+prostraction@users.noreply.github.com>
2023-04-03 16:21:05 +0300

    Set Routes and add Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
-import React, {Component} from "react";
-import './index.css'
+import React, {Component, Fragment} from "react";
+import { BrowserRouter as Rounter, Routes, Route, Link, useParams } from "react-router-dom";
+
+import Home from './components/Home'
+import Admin from './components/Admin'
+import TrackNumber from './components/TrackNumber'
 
 export default class App extends Component {
     render() {
         return (
-            <h1> ok </h1>
+            <Rounter>
+            <div className="container">
+                <div className="row">
+                    <h1 className="mt-1">
+                        Test
+                    </h1>
+                    <hr className="mb-3"></hr>
+                </div>
+
+                <div className="row">
+                    <div className="col-md-2">
+                        <nav>
+                            <ul className="list-group">
+                                <li className="list-group-item">
+                                    <Link to="/">Home</Link>
+                                </li>
+                                <li className="list-group-item">
+                                    <Link to="/admin">Admin</Link>
+                                </li>
+                                <li className="list-group-item">
+                                    <Link to="/track-number">Track number</Link>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+
+                    <div className="col-md-10">
+                        <Routes>
+                            <Route path='/' element={<Home/>}/>
+                            <Route path='/admin' element={<Admin/>}/>
+                            <Route path='/track-number/:id' element={<Number/>}/>
+                            <Route path='/track-number' element={<TrackNumber/>}/>
+                        </Routes>
+                    </div>
+                </div>
+            </div>
+            </Rounter>
         )
     }
+}
+
+function Number() {
+    let {id} = useParams();
+    return <h2>Number id = {id} </h2>
 }
